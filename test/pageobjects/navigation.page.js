af7c77668ca17c775b27d2351974cc65b35bec2f
Elvin Gómez <el_gch24@hotmail.com>
2020-07-31 19:49:11 -0600

    fix Sintaxis

--- test/pageobjects/navigation.page.js
+++ test/pageobjects/navigation.page.js
@@ -1,12 +1,7 @@
 const Page = require('./page');
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class navigationPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     get conduitLabel () { return $('a.navbar-brand.ng-binding') }
     get signInLink () { return $('=Sign in') }
     get signUpLink () { return $('*=up') }
